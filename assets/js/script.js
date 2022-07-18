7777c67ae79a120127cc41fe8a2f2bf5a0cd7b54
RKH1988 <regettaspearman@gmail.com>
2022-07-18 02:19:42 -0400

    added fetch for google api

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ var eventSubmitHandler = function(event) {
 }
 
 
-eventFormEl.addEventListener("submit", eventSubmitHandler);
+//eventFormEl.addEventListener("submit", eventSubmitHandler);
 
 //fetch(`https://api.seatgeek.com/2/events?geoip=98.213.245.205&range=12mi&client_id=Mjc4NjY0OTJ8MTY1Nzg0MTg4Ni44MzQwMTky`)
 //.then(res => res.json())
@@ -39,3 +39,36 @@ eventFormEl.addEventListener("submit", eventSubmitHandler);
 //  console.log(data);
 //})
 //.catch(err => console.log(err));
+
+
+
+var displayDirections = function() {
+    var apiStart="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin="
+    var key="&key=AIzaSyD977niwAg_ga4uwIxlClUPRMYJ9IcsNCA"
+    var apiMiddle="&destination="
+    var origin = "Greensboro";
+    var destination = "Kernersville";
+    var apiUrl=apiStart+origin+apiMiddle+destination+key;
+
+    fetch(apiUrl).then(function(response){
+        var directionsListEl= document.querySelector("#directionsList");
+        var directionsLi=document.createElement("li");
+        if(response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+
+                console.log(data.routes[0].legs[0].steps.length);
+                for (var i=0; i<data.routes[0].legs[0].steps.length; i++) {            
+                    console.log(data.routes[0].legs[0].steps[i].html_instructions);
+                    directionsLi.innerHTML=data.routes[0].legs[0].steps[i].html_instructions;
+                    directionsListEl.appendChild(directionsLi);
+                }
+                return directionsListEl;
+            })
+        } else {
+            console.log("Not Working");
+        }
+    })
+};
+
+displayDirections();
